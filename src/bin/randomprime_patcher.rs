4da5d7e930d56cd310b95331ecedf004141c4023
Blake Spangenberg <bspangenberg@insteon.com>
2021-03-02 08:12:24 -0800

    added struct definition for water

--- src/bin/randomprime_patcher.rs
+++ src/bin/randomprime_patcher.rs
@@ -342,7 +342,7 @@ fn get_config() -> Result<patches::ParsedConfig, String>
         None
     };
 
-    let mpdr_version = "Plando v1.6";
+    let mpdr_version = "Plando v1.7";
     let mut comment_message:String = "Generated with ".to_owned();
     comment_message.push_str(mpdr_version);
 
