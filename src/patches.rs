a6e020025fa2f5d927bd1eaf0c293a77eda666c7
toasterparty <toasterparty@derpymail.org>
2021-07-16 23:23:06 -0700

    fix vault bomb slots not ejecting players properly

--- src/patches.rs
+++ src/patches.rs
@@ -2775,7 +2775,7 @@ fn patch_remove_cutscenes(
             }
 
             // ball triggers can be mean sometimes when not in the saftey of a cutscene, tone it down from 40 to 10
-            if obj.property_data.is_ball_trigger() {
+            if obj.property_data.is_ball_trigger() && room_id != 0xEF069019 {
                 let ball_trigger = obj.property_data.as_ball_trigger_mut().unwrap();
                 ball_trigger.force = 10.0;
             }
