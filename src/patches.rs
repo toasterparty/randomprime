e9b07d75163fd490d5b63e67f9cb61d504d46f31
UltiNaruto <zenaruto@ymail.com>
2022-07-10 19:19:04 +0200

    Implemented hide item dots dol patch (#56)
    * Implemented hide item dots dol patch
    Added cmplw command in ppcasm module
    Added cmpw command in ppcasm module
    Fixed some indentation for progressive suit damage patch
    Added an helper function to see if a memory relay is active
    
    * Fixed item dots crash for PAL and NTSC-J version

--- src/patches.rs
+++ src/patches.rs
@@ -8005,7 +8005,7 @@ fn patch_dol<'r>(
                 rlwinm  r4, r4, 2, 0, 29;
                 lis     r6, data@h;
                 addi    r6, r6, data@l;
-                lfsx     f0, r4, r6;
+                lfsx    f0, r4, r6;
                 b       { symbol_addr!("ApplyLocalDamage__13CStateManagerFRC9CVector3fRC9CVector3fR6CActorfRC11CWeaponMode", version) + jump_offset };
             data:
                 .float 0.0;
@@ -8211,6 +8211,62 @@ fn patch_dol<'r>(
 
     new_text_section_end = new_text_section_end + rel_loader_size;
 
+    let is_memory_active_func = new_text_section_end;
+    let is_memory_active_func_patch = ppcasm!(is_memory_active_func, {
+        // function header
+        stwu      r1, -0x24(r1);
+        mflr      r0;
+        stw       r0, 0x24(r1);
+        stw       r14, 0x20(r1);
+        stw       r15, 0x1c(r1);
+        stw       r29, 0x18(r1);
+        mr        r29, r6;
+        stw       r30, 0x14(r1);
+        mr        r30, r4;
+        stw       r31, 0x10(r1);
+        mr        r31, r3;
+
+        // function body
+        lis       r3, { symbol_addr!("g_GameState", version) }@h;
+        addi      r3, r3, { symbol_addr!("g_GameState", version) }@l;
+        lwz       r3, 0x0(r3);
+        bl        { symbol_addr!("CurrentWorldState__10CGameStateFv", version) };
+        lwz       r14, 0x08(r3);
+        lwz       r14, 0x00(r14);
+        li        r0, 0;
+        li        r3, 1;
+        lwz       r6, 0x00(r14);
+        addi      r6, r6, 1;
+        cmpw      r3, r6;
+        bge       { is_memory_active_func + 0x80 };
+        rlwinm    r3, r3, 2, 0, 29;
+        lwzx      r15, r3, r14;
+        rlwinm    r3, r3, 30, 4, 31;
+        cmpw      r15, r31;
+        bne       { is_memory_active_func + 0x78 };
+        li        r0, 1;
+        b         { is_memory_active_func + 0x80 };
+        addi      r3, r3, 1;
+        b         { is_memory_active_func + 0x54 };
+        mr        r3, r0;
+
+        // function footer
+        lwz       r0, 0x24(r1);
+        lwz       r14, 0x20(r1);
+        lwz       r15, 0x1c(r1);
+        mr        r6, r29;
+        lwz       r29, 0x18(r1);
+        mr        r4, r30;
+        lwz       r30, 0x14(r1);
+        lwz       r31, 0x10(r1);
+        mtlr      r0;
+        addi      r1, r1, 0x24;
+        blr;
+    });
+
+    new_text_section_end = new_text_section_end + is_memory_active_func_patch.encoded_bytes().len() as u32;
+    new_text_section.extend(is_memory_active_func_patch.encoded_bytes());
+
     let patch_pickup_icon_case = ppcasm!(symbol_addr!("Case1B_Switch_Draw__CMappableObject", version) + ((structs::MapaObjectType::Pickup as u32) - 0x1b) * 4, {
             .long         new_text_section_end;
     });
@@ -8218,21 +8274,48 @@ fn patch_dol<'r>(
 
     // Pattern to find CMappableObject::Draw(int, const CMapWorldInfo&, float, bool)
     // 2c070000 7c????78 38000000
-    // TO-DO check if memory relay of the mappable object to draw activated
-    let off = if version == Version::Pal || version == Version::NtscJ {
-        0x284
+    let off = if version == Version::Pal {
+        -0x5e3c
+    } else if version == Version::NtscJ {
+        -0x5e64
     } else {
-        0x298
+        -0x5eb4
     };
-    let set_pickup_icon_txtr_patch = ppcasm!(new_text_section_end, {
-            lwz          r3, -0x5eb4(r13);
+
+    if version == Version::NtscJ || version == Version::Pal {
+        let set_pickup_icon_txtr_patch = ppcasm!(new_text_section_end, {
+            lwz          r3, 0x08(r18);
+            bl           { is_memory_active_func };
+            lis          r31, { custom_asset_ids::MAP_PICKUP_ICON_TXTR.to_u32() }@h;
+            addi         r31, r31, { custom_asset_ids::MAP_PICKUP_ICON_TXTR.to_u32() }@l;
+            mr           r0, r31;
+            cmpwi        r3, 0;
+            lis          r31, 0xffff;
+            ori          r31, r31, 0xffff;
+            lwz          r3, { off }(r13);
+            beq          { new_text_section_end + 0x2c };
+            fmr          f30, f14;
+            b            { symbol_addr!("Draw__15CMappableObjectCFiRC13CMapWorldInfofb", version) + 0x284 };
+        });
+
+        new_text_section_end = new_text_section_end + set_pickup_icon_txtr_patch.encoded_bytes().len() as u32;
+        new_text_section.extend(set_pickup_icon_txtr_patch.encoded_bytes());
+    } else {
+        let set_pickup_icon_txtr_patch = ppcasm!(new_text_section_end, {
+            lwz          r3, 0x08(r18);
+            bl           { is_memory_active_func };
+            cmpwi        r3, 0;
+            lwz          r3, { off }(r13);
             lis          r6, { custom_asset_ids::MAP_PICKUP_ICON_TXTR.to_u32() }@h;
             addi         r6, r6, { custom_asset_ids::MAP_PICKUP_ICON_TXTR.to_u32() }@l;
-            b            { symbol_addr!("Draw__15CMappableObjectCFiRC13CMapWorldInfofb", version) + off };
-    });
+            beq          { new_text_section_end + 0x20 };
+            fmr          f30, f14;
+            b            { symbol_addr!("Draw__15CMappableObjectCFiRC13CMapWorldInfofb", version) + 0x298 };
+        });
 
-    new_text_section_end = new_text_section_end + set_pickup_icon_txtr_patch.encoded_bytes().len() as u32;
-    new_text_section.extend(set_pickup_icon_txtr_patch.encoded_bytes());
+        new_text_section_end = new_text_section_end + set_pickup_icon_txtr_patch.encoded_bytes().len() as u32;
+        new_text_section.extend(set_pickup_icon_txtr_patch.encoded_bytes());
+    }
 
     if config.warp_to_start
     {
