62db4142090d053cb2d48b02fa2f3054bd40ef40
toasterparty <toasterparty@derpymail.org>
2023-09-02 15:29:31 -0700

    call it index

--- ci.sh
+++ ci.sh
@@ -9,6 +9,6 @@ python -m pip install json-schema-for-humans==0.45.1
 echo "Creating Schema HTML"
 rm -rf ./schema/build
 mkdir -p ./schema/build
-generate-schema-doc --config-file ./schema/config.json ./schema/randomprime.schema.json ./schema/build/randomprime.html
+generate-schema-doc --config-file ./schema/config.json ./schema/randomprime.schema.json ./schema/build/index.html
 
 cp ./schema/_config.yml ./schema/build
